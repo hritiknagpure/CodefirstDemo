e971f2538768c26cbb71bfbbcadde95c8f9a1b1a
hriti <hriti@HRITIKNAGPURE>
2024-12-09 12:52:11 +0530

    Add project files.

---
+++ CodefirstDemo/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodefirstDemo.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("EmployeName", TypeName ="varchar(100)")]
+        public string name { get; set; }
+        public int age { get; set; }
+        public string phone { get; set; }   
+
+        public string email { get; set; }
+        public string phoneNumber { get; set; } = string.Empty;
+        public string city { get; set; }
+        public string country { get; set; }
+        public string zipcode { get; set; }
+        public string city_name { get; set;
+    }
+        public string country_name { get; set; }
+}
+    }
